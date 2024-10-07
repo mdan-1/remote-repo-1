2668bd80731ca75bebd4a24bfbac190045760c29
mdan-1 <blueminer111@gmail.com>
2024-10-06 20:55:14 -0400

    initial commit

---
+++ Hello.py
@@ -0,0 +1,13 @@
+def addTwoNumbers(n1, n2):
+    return n1+n2
+def multiplyTwoNumbers(x, b):
+    return x * b
+
+x = 5
+b = 5
+sumValue =  addTwoNumbers(x,b)
+multiplyValue = multiplyTwoNumbers(x, b)
+
+
+print(f"{x} + {b} = \n {sumValue}")
+print(f"{x} * {b} = \n {multiplyValue}")
---
+++ Javascript.js
@@ -0,0 +1,22 @@
+async (params) => {
+    
+}
+async function name(params) {
+    
+}
+function countDown(value) {
+    while (value>=0) {
+        console.log(value);
+        value--;
+    }
+}
+function CountUp(value) {
+    
+}
+//press ctrl+space to reopen the intelliSense list after period.
+
+let address = '7922 Cedar View Blvd';
+console.log('File: + Javascript.js');
+console.log('address = ' + address);
+
+
